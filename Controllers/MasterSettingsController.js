b9817dd691c17d8c6a24fd5478dff542abafc696
codeclouds-bidyut <bidyut.patra@codeclouds.com>
2024-09-30 14:23:08 +0530

    Backend Settings

--- Controllers/MasterSettingsController.js
+++ Controllers/MasterSettingsController.js
@@ -38,6 +38,22 @@ class MasterSettingsController extends Controller {
           }
     }
 
+    async view(req, res) {
+        const { key } = req.query;
+        try {
+          const data = await MasterSetting.findOne(
+            { key: key },
+            { key: 1, value: 1, _id: 1 }
+          );
+          return data;
+        } catch (error) {
+          res.status(500).json({
+            status: false,
+            message: error.message,
+          });
+        }
+      }
+
 }
 
 module.exports = MasterSettingsController;
